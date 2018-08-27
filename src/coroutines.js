670e5b2ab1bd7736171604d3ab8e7f53ce0e5fca
Pascal Bugnion <pascal@bugnion.org>
2018-08-27 15:24:17 +0100

    Format code with prettier

--- src/coroutines.js
+++ src/coroutines.js
@@ -1,11 +1,11 @@
 //@flow
 
-import fs from 'fs'
-import path from 'path'
-import os from 'os'
+import fs from "fs";
+import path from "path";
+import os from "os";
 
 // Directory to read files from
-const root = os.homedir()
+const root = os.homedir();
 
 /** Helper method to create and initialize a coroutine.
  *
@@ -15,65 +15,61 @@ const root = os.homedir()
  * it to the first yield and returns it.
  */
 function coroutine<G: Generator<any, any, any>>(
-  generatorFunction: ((...args: Array<any>) => G)
+  generatorFunction: (...args: Array<any>) => G
 ) {
   return function(...args: Array<any>): G {
-    const generator = generatorFunction(...args)
-    generator.next()
-    return generator
-  }
+    const generator = generatorFunction(...args);
+    generator.next();
+    return generator;
+  };
 }
 
 /** Coroutine that reads the files within `directory` and feeds them onwards
  *
  * @param {string} directory Directory to read files from
  * @param {Coroutine} target Coroutine that accepts the files
-*/
+ */
 const pushFiles = function(
   directory: string,
   target: Generator<any, any, string>
 ): void {
-  fs.readdir(
-    directory,
-    { encoding: 'utf-8' },
-    (error, fileNames) => {
-      if (error) {
-        throw error
-      } else {
-        for (const fileName of fileNames) {
-          const filePath = path.join(directory, fileName)
-          target.next(filePath)
-        }
+  fs.readdir(directory, { encoding: "utf-8" }, (error, fileNames) => {
+    if (error) {
+      throw error;
+    } else {
+      for (const fileName of fileNames) {
+        const filePath = path.join(directory, fileName);
+        target.next(filePath);
       }
     }
-  )
-}
+  });
+};
 
 /** Coroutine that forwards a file path on if it is a file.
  *
  * @param {Coroutine} target Coroutine to send files to
  */
-const isFile: (Generator<any, any, string> => Generator<void, void, string>) =
-  coroutine(function* (target) {
-    while (true) {
-      const fileMaybe: string = yield
-      fs.lstat(fileMaybe, (error, stat) => {
-        if (error) {
-          throw error
-        } else if (stat.isFile()) {
-          target.next(fileMaybe)
-        }
-      })
-    }
-  })
+const isFile: (
+  Generator<any, any, string>
+) => Generator<void, void, string> = coroutine(function*(target) {
+  while (true) {
+    const fileMaybe: string = yield;
+    fs.lstat(fileMaybe, (error, stat) => {
+      if (error) {
+        throw error;
+      } else if (stat.isFile()) {
+        target.next(fileMaybe);
+      }
+    });
+  }
+});
 
 /** Coroutine that logs the information it receives */
-const log: () => Generator<void, void, string> = coroutine(function* () {
+const log: () => Generator<void, void, string> = coroutine(function*() {
   while (true) {
-    const item: string = yield
-    console.log(item)
+    const item: string = yield;
+    console.log(item);
   }
-})
+});
 
-
-pushFiles(root, isFile(log()))
+pushFiles(root, isFile(log()));
--- src/generators.js
+++ src/generators.js
@@ -1,51 +1,46 @@
 //@flow
 
-import fs from 'fs'
-import path from 'path'
-import os from 'os'
+import fs from "fs";
+import path from "path";
+import os from "os";
 
 // The root directory from which to run the demo.
 // Change this to a directory that actually exists on your computer
-const rootDirectory = path.join(os.homedir(), 'test/tmp-108')
+const rootDirectory = path.join(os.homedir(), "test/tmp-108");
 
 /** Recursively walk directories.
-  *
-  * Returns an iterator of paths to files located below the
-  * directory passed as argument.
-  *
-  * @param {string} root Directory to start walking from.
-  * @return {Gernator<string, void, void>} Generator of file paths below root.
-  */
+ *
+ * Returns an iterator of paths to files located below the
+ * directory passed as argument.
+ *
+ * @param {string} root Directory to start walking from.
+ * @return {Gernator<string, void, void>} Generator of file paths below root.
+ */
 function* walkDirectories(root: string): Generator<string, void, void> {
   for (const name of fs.readdirSync(root)) {
-    const filePath = path.join(root, name)
-    const stat = fs.lstatSync(filePath)
+    const filePath = path.join(root, name);
+    const stat = fs.lstatSync(filePath);
     if (stat.isFile()) {
-      yield filePath
+      yield filePath;
     } else if (stat.isDirectory()) {
-      yield* walkDirectories(filePath)
+      yield* walkDirectories(filePath);
     }
   }
 }
 
 /** Count the number of files by extension.
-  *
-  * @param {Iterator<string>} files Iterator of file names
-  * @return {Object} Map of extension to the number of times this extension occurs
-  */
-function countFilesByExtension(files: Iterator<string>): {[string]: number} {
+ *
+ * @param {Iterator<string>} files Iterator of file names
+ * @return {Object} Map of extension to the number of times this extension occurs
+ */
+function countFilesByExtension(files: Iterator<string>): { [string]: number } {
   let total = {};
   for (const f of files) {
-    const extension = path.extname(f)
-    const currentCount = total[extension] || 0
-    total[extension] = currentCount + 1
+    const extension = path.extname(f);
+    const currentCount = total[extension] || 0;
+    total[extension] = currentCount + 1;
   }
-  return total
+  return total;
 }
 
-console.log(
-  countFilesByExtension(
-    walkDirectories(rootDirectory)
-  )
-)
-
+console.log(countFilesByExtension(walkDirectories(rootDirectory)));
